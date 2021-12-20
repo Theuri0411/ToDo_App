2fcd5af1cc2978cfb8da2ff6a366f052130349aa
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-12-20 20:30:49 +0300

    updatw

--- notion.py
+++ notion.py
@@ -11,6 +11,60 @@ class NotionClient:
             "Content-Type" : "application/json",
             "Notion-Version" : "2021-08-16"
         }
-    def create_page (self, decription, date, status):
-        pass
-    
+    def create_page (self, description, date, status):
+        create_url = "https://api.notion.com/v1/pages"
+
+
+        data = {
+            "parent": { "database_id":self.database_id },
+            "properties": {
+                "Description": {
+                    "title": [
+                        {
+                            "text": {
+                                "content": description
+                            }
+                        }
+                    ]
+                },
+                "Date": {
+                    "date": [
+                        {
+                            "text": {
+                                "content": date
+                            }
+                        }
+                    ]
+                },
+                "Food group": {
+                    "select": {
+                        "name": "Vegetable"
+                    }
+                },
+                "Price": { "number": 2.5 }
+            },
+            "children": [
+                {
+                    "object": "block",
+                    "type": "heading_2",
+                    "heading_2": {
+                        "text": [{ "type": "text", "text": { "content": "Lacinato kale" } }]
+                    }
+                },
+                {
+                    "object": "block",
+                    "type": "paragraph",
+                    "paragraph": {
+                        "text": [
+                            {
+                                "type": "text",
+                                "text": {
+                                    "content": "Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.",
+                                    "link": { "url": "https://en.wikipedia.org/wiki/Lacinato_kale" }
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        }'
