5eb31bbebf271dff72a5d561c9827927e9a82757
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-12-20 19:08:11 +0300

    Created Functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Text
 import speech_recognition as sr
 import gtts
 from playsound import playsound
@@ -10,3 +11,17 @@ def get_audio():
         print ("Say Something")
         audio = r.listen(source)
     return audio
+
+def audio_to_text (audio):
+    text = ""
+    try:
+        text= r.recognize_google(audio)
+    except sr.UnknownValueError:
+        print("Speech Could Not Understand Audio")
+    except sr.RequestError:
+        print("Could Not Process Results from API")
+    return text
+
+
+    if __name__ == "__main__":
+        a = get audio
